243b175eff34ee96dd00e7a37b76826d7d7a37e6
Tim <tim@tim.ru>
2026-03-24 18:59:05 +0500

    6 done

--- express-app/routes/users.js
+++ express-app/routes/users.js
@@ -10,11 +10,20 @@ let lastId = 2;
 
 /* GET users listing. */
 router.get('/', function(req, res, next) {
-  res.send(
+  res.json(
     {items: users}
   );
 });
 
+router.get('/:id', function(req, res, next) {
+  const id = parseInt(req.params.id);
+  const user = users.find(u => u.id === id);
+  
+  if (!user) {
+    return res.status(404).send('User not found');
+  }
+  res.json(user);
+});
 
 router.post('/',function(req, res, next){
   const { name } = req.body;
