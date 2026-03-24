756efd44396781130e0910dbde5269336ee28286
Tim <tim@tim.ru>
2026-03-24 18:49:40 +0500

    5 done

--- express-app/routes/users.js
+++ express-app/routes/users.js
@@ -1,22 +1,29 @@
 const express = require('express');
 const router = express.Router();
 
+let users = [
+  { id: 1, name: "Ильин Тимофей" },
+  { id: 2, name: "Мишуков Егор" }
+];
+let lastId = 2;
+
+
 /* GET users listing. */
 router.get('/', function(req, res, next) {
   res.send(
-    {
-      items: [
-        {
-          id: 1,
-          name: "Ильин Тимофей"
-        },
-        {
-          id: 2,
-          name: "Мишуков Егор"
-        }
-      ]
-    }
+    {items: users}
   );
+});
+
+
+router.post('/',function(req, res, next){
+  const { name } = req.body;
+  const newUser = {
+    id: ++lastId,
+    name: name
+  };
+  users.push(newUser);
+  res.status(201).json(newUser);
 });
 
 module.exports = router;
